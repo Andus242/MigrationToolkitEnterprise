f2420367c143388366a27da1fcc6bce5b696684b
Andus242 <andus1@netactive.co.za>
2026-09-09 22:18:24 +0200

    Fix application settings destination path

--- src/MTE.Engine/Services/ApplicationSettingsMigrationService.cs
+++ src/MTE.Engine/Services/ApplicationSettingsMigrationService.cs
@@ -63,6 +63,7 @@ public sealed class ApplicationSettingsMigrationService
             var destinationFolder =
                 Path.Combine(
                     destinationProfile,
+                    "AppData",
                     folderName);
 
             if (!Directory.Exists(sourceFolder))
